56efb4c73cb503dbc175d01194e30ed093ec7f1f
Dita rector <rectordita@gmail.com>
2026-03-17 19:13:17 +0700

    CourseEnpoint Implemented

--- src/main/java/com/HRD/DitaRector/PVH/Spring/repository/CourseRepository.java
+++ src/main/java/com/HRD/DitaRector/PVH/Spring/repository/CourseRepository.java
@@ -1,4 +1,22 @@
 package com.HRD.DitaRector.PVH.Spring.repository;
 
+import com.HRD.DitaRector.PVH.Spring.model.Entity.Course;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+@Mapper
 public interface CourseRepository {
+    @Results(id = "courseMapper", value = {
+            @Result(property = "courseId" , column = "course_id"),
+            @Result(property = "courseName" , column = "course_name")
+//            @Result(property = "description" , column = "description")
+    })
+    @Select(
+    "SELECT *  FROM courses OFFSET #{offset} LIMIT #{size}")
+    List<Course> getAllCourse(Integer offset, Integer size);
+
+
 }
